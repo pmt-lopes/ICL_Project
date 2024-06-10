7880565f0a6e9ee7d0e7929ed094d54cdc3455dd
lowdon <franciscogalliscosta@live.com.pt>
2024-06-10 23:45:05 +0100

    final merged

--- src/interpreter/Interpreter.java
+++ src/interpreter/Interpreter.java
@@ -278,23 +278,7 @@ public class Interpreter implements ast.Exp.Visitor<Value,Env<Value>>{
 		return e.accept(i, env);
 	}
 
-	@Override
-	public Value visit(ASTIf e, Env<Value> env) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public Value visit(ASTPrint e, Env<Value> env) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Value visit(ASTPrintln e, Env<Value> env) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 
 }
